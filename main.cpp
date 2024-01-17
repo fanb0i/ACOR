7ff3a7e35ecad2c7519e8e7e29eb1e0a0280482f
Anis Zeghlouche <aniszeghlouche06@gmail.com>
2024-01-17 21:00:26 +0100

    B

--- main.cpp
+++ main.cpp
@@ -20,9 +20,101 @@ double fonctionRastrigin(std::vector<double> x)
 
 	return sum;
 }
+
+//Rosenbrock function
+double rosenbrock(const std::vector<double>& x) {
+    double sum = 0.0;
+    for (size_t i = 0; i < x.size() - 1; ++i) {
+        double term = 100.0 * pow(x[i + 1] - pow(x[i], 2), 2) + pow(1 - x[i], 2);
+        sum += term;
+    }
+    return sum;
+}
+
+//Rastrigin function
+double rastrigin(const std::vector<double>& x) {
+    size_t n = x.size();
+    double A = 10.0;
+    double sum = A * n;
+
+    for (size_t i = 0; i < n; ++i) {
+        sum += pow(x[i], 2) - A * cos(2 * M_PI * x[i]);
+    }
+
+    return sum;
+}
+
+//Weierstrass function
+double weierstrass(const std::vector<double>& x) {
+    int kMax = 20;
+    double a = 0.5;
+    double b = 3.0;
+    double sum = 0.0;
+
+    for (int k = 0; k <= kMax; ++k) {
+        sum += pow(a, k) * cos(2.0 * M_PI * pow(b, k) * (x[0] + 0.5));
+    }
+
+    return sum;
+}
+
+//Michalewicz function
+double michalewicz(const std::vector<double>& x) {
+    double sum = 0.0;
+    double m = 10.0;
+    for (size_t i = 0; i < x.size(); ++i) {
+        sum += sin(x[i]) * pow(sin((i + 1) * pow(x[i], 2) / M_PI), 2 * m);
+    }
+
+    return -sum; // Minimization problem, so negating the result.
+}
+
+//Griewank
+double griewank(const std::vector<double>& x) {
+    double sum = 0.0;
+    double product = 1.0;
+
+    for (size_t i = 0; i < x.size(); ++i) {
+        sum += pow(x[i], 2) / 4000.0;
+        product *= cos(x[i] / sqrt(i + 1.0));
+    }
+
+    return 1.0 + sum - product;
+}
+
+//Ackley function
+double ackley(const std::vector<double>& x) {
+    const double M_E = std::exp(1.0);
+
+    size_t n = x.size();
+    double sum1 = 0.0;
+    double sum2 = 0.0;
+
+    for (size_t i = 0; i < n; ++i) {
+        sum1 += pow(x[i], 2);
+        sum2 += cos(2.0 * M_PI * x[i]);
+    }
+
+    return -20.0 * exp(-0.2 * sqrt(sum1 / n)) - exp(sum2 / n) + 20.0 + M_E;
+}
+
+//schwefel function
+double schwefel(const std::vector<double>& x) {
+    double sum = 0.0;
+
+    for (size_t i = 0; i < x.size(); ++i) {
+        sum += x[i] * sin(sqrt(fabs(x[i])));
+    }
+
+    return -sum; // Minimization problem, so negating the result.
+}
+
+#include <cmath>
+
+
 int main()
 {
-	Problem* p = new Problem{30,-5.12,5.12,fonctionRastrigin,"min"};
+	Problem* p = new Problem{30,-5.12,5.12,ackley,"min"};
 	OriginalACOR acor(10000, 30, 20, 0.5, 1.0, 3.15);
 	Agent g_best = acor.solve(p);
 	std::cout << g_best.getfitness();
